c3f501f1603692e91e3632f112bc973746f23e74
kadhitz-sa <khadeejafarook01@gmail.com>
2026-07-03 09:36:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Welcome to Ayisha ❤️ Muhammed Razi's Wedding Website");
+const weddingDate = new Date("August 09, 2026 11:00:00").getTime();
+
+setInterval(function () {
+
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
